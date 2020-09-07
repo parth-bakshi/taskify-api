31f46dbe1f28a49691ca35b191c3172b89ebce21
prateek-garg-mnr <prateek.garg@mckinleyrice.com>
2020-09-08 00:14:21 +0530

    Data validation checks added in user schema

--- model/user.js
+++ model/user.js
@@ -1,23 +1,48 @@
-const mongoose = require('mongoose');
-const db = require('../config/mongoose');
-const userSchema = new mongoose.Schema({
-    email:{
-        type:String,
-        required:true,
-        unique:true
+const mongoose = require("mongoose");
+const db = require("../config/mongoose");
+const validator = require("validator");
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (validator.isEmail(value)) {
+          throw new Error("Enter a valid email");
+        }
+      },
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      trim: true,
+      validate(value) {
+        if (value.toLowerCase().includes("password")) {
+          throw new Error("Password can't contain password");
+        }
+      },
     },
-    name:{
-        type:String,
-        required:true
-    }
-},{
-    timestamps:true
-});
+    name: {
+      type: String,
+      required: true,
+    },
+    tokens: [
+      {
+        token: {
+          type: string,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const User = mongoose.model('User',userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
