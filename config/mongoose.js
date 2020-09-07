f89ebf45ba1dfd794d79763e2c2b81acfe0b80e3
prateek-garg-mnr <prateek.garg@mckinleyrice.com>
2020-09-08 00:12:30 +0530

    mongodb atlas connected

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost/todo-mckinley");
+mongoose.connect("mongodb+srv://taskifymnr:AGI1i532Ps39w1kP@taskify-mnr.uhcio.mongodb.net/taskify?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.on('error',console.error.bind("error creating db"));
@@ -9,6 +9,7 @@ db.once('open',function(err){
         console.log('err opening db');
         return;
     }
+    console.log('Connected To Atlas')
 });
 
 module.exports = db;
--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-let express = require("express");
-let app = express();
-const port = 8000;
+// Express
+const express = require("express");
+// Express App
+const app = express();
+// Passport
+const passport = require("passport")
 
-let db = require("./config/mongoose");
+// PORT
+const port = process.env.PORT||8000;
+
+const db = require("./config/mongoose");
+
+// Parse from request
+app.use(express.urlencoded({ extended: true }));
+
 
 app.listen(port,function(err){
     if(err){console.log(err);return;}
