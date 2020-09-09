b3a0acafba0d9405b4747145cc7e582d80f243b9
parth-bakshi <parth.bakshi@mckinleyrice.com>
2020-09-09 21:35:13 +0530

    [test]check added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/",function(req,res){
+    res.send("working");
+})
 router.use("/user", require("./user"));
 router.use("/task",require("./task"))
 
