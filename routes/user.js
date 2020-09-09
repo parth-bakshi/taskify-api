b734c5e91d25e0a6cfa233c6491cd66827f4b2e3
parth-bakshi <parth.bakshi@mckinleyrice.com>
2020-09-09 21:11:27 +0530

    [test] check added

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const auth = require("../config/auth");
 const userController = require("../controllers/userController");
 
+router.get("/check",function(req,res){
+    res.send("hi");
+})
 router.get("/", auth, userController.userData);
 router.post("/create", userController.create);
 router.post("/login", userController.login);
