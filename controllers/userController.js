6f56d5be52f59d87fb2bf211114938a93cdba6fb
prateek-garg-mnr <prateek.garg@mckinleyrice.com>
2020-09-09 22:05:53 +0530

    logout api

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,10 +8,10 @@ module.exports.create = async (req, res) => {
       password: req.body.password,
       name: req.body.name,
     });
-    const token = await user.generateAuthToken()
+    const token = await user.generateAuthToken();
 
     await user.save();
-    res.status(201).send({ user,token });
+    res.status(201).send({ user, token });
   } catch (e) {
     console.log("Error in Create API: ", e);
     if ((e.code = 11000)) {
@@ -22,36 +22,48 @@ module.exports.create = async (req, res) => {
 
 module.exports.login = async (req, res) => {
   try {
+    console.log(req.body);
     const user = await User.findByCredentials(
       req.body.email,
       req.body.password
     );
-    const token = await user.generateAuthToken()
-    res.send({ user,token });
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
   } catch (e) {
     console.log("Error In Login API: ", e);
     res.status(400).send({ message: "Unable to Login" });
   }
 };
 
-
-module.exports.userData = async(req,res)=>{
-    try{    
-        res.send({user:req.user})
-    } catch(e){
-        console.log('User Data API: ',e)
-        res.status(500).send({message:"Unable to fetch"})
-    }
-}
-
-module.exports.createCategory = async(req,res)=>{
-  try{
-    let categories = [...req.user.categories,req.body.category]
-    await req.user.update({categories})
-    await req.user.save()
-    return res.send({message:"Category Updated"})
-  } catch(e){
-    console.log('Create Category API: ',e)
-    res.status(500).send({message:"Unable to Create Category"})
+module.exports.logout = async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+    res.send({ message: "Logged Out Successfully" });
+  } catch (e) {
+    res.status(500).send();
   }
-}
+};
+
+module.exports.userData = async (req, res) => {
+  try {
+    res.send({ user: req.user });
+  } catch (e) {
+    console.log("User Data API: ", e);
+    res.status(500).send({ message: "Unable to fetch" });
+  }
+};
+
+module.exports.createCategory = async (req, res) => {
+  try {
+    let categories = [...req.user.categories, req.body.category];
+    await req.user.update({ categories });
+    await req.user.save();
+    return res.send({ message: "Category Updated" });
+  } catch (e) {
+    console.log("Create Category API: ", e);
+    res.status(500).send({ message: "Unable to Create Category" });
+  }
+};
--- routes/user.js
+++ routes/user.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const auth = require("../config/auth");
 const userController = require("../controllers/userController");
 
-router.get("/check",function(req,res){
-    res.send("hi");
-})
+router.get("/check", function (req, res) {
+  res.send("hi");
+});
 router.get("/", auth, userController.userData);
 router.post("/create", userController.create);
 router.post("/login", userController.login);
-router.post("/create/category",auth,userController.createCategory)
+router.post("/logout", auth, userController.logout);
+router.post("/create/category", auth, userController.createCategory);
 
 module.exports = router;
