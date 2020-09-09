005f47e8bb1b7b77a28ead23c2b8ffb780b51117
parth-bakshi <parth.bakshi@mckinleyrice.com>
2020-09-10 03:22:08 +0530

    [feature] add api for returning all tasks

--- routes/task.js
+++ routes/task.js
@@ -6,4 +6,7 @@ const auth = require("../config/auth");
 // create task
 router.post("/create", auth, taskController.createTask);
 
+//return all tasks
+router.get("/get-all-tasks", auth, taskController.getTasks);
+
 module.exports = router;
