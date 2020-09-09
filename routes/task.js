7c5de87b453e1e45ca5c3fdf280b615d158293ef
parth-bakshi <parth.bakshi@mckinleyrice.com>
2020-09-10 03:22:41 +0530

    [feature] add api for deleting a particular task

--- routes/task.js
+++ routes/task.js
@@ -9,4 +9,7 @@ router.post("/create", auth, taskController.createTask);
 //return all tasks
 router.get("/get-all-tasks", auth, taskController.getTasks);
 
+//delete a task
+router.post("/delete-task",auth,taskController.deleteTask);
+
 module.exports = router;
